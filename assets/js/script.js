bb4b91495d2d504f128ac9a4519afb73c56b1b5a
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2020-12-28 22:07:43 -0600

    added questions/added timer penalty

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,36 +6,10 @@ const questionEl = document.getElementById('question');
 const choiceA = document.getElementById('A');
 const choiceB = document.getElementById('B');
 const choiceC = document.getElementById('C');
-const choiceD = document.getElementById('D');
+const choiceD = document.getElementById('D'); 
+let timeLeft = 75;
 
-
-// CHECKOUT DRUMBEAT TO STOP COUNTER FROM STARTING OVER MULTIPLE TIMES
-// Timer that counts down from 75
-function countdown() {
-  let timeLeft = 75;
-
-  // setInterval to 1000ms. timer changes color to alert user that time is running out.
-  let timeInterval = setInterval(function() {
-    if( timeLeft > 30){
-        timerEl.innerHTML = "<h4 id='timer'>Time Left: <span style='color: darkgreen;'>" + timeLeft + "</span></h4>";
-        
-        timeLeft--;
-    }else if(timeLeft > 10){
-        timerEl.innerHTML = "<h4 id='timer'>Time Left: <span style='color: orange;'>" + timeLeft + "</span></h4>";
-
-        timeLeft--;
-    }else if(timeLeft >= 1){
-        timerEl.innerHTML = "<h4 id='timer'>Time Left: <span style='color: red;'>" + timeLeft + "</span></h4>";
-
-        timeLeft--;
-    }else{
-        timerEl.textContent = 'Timer: 0';
-        clearInterval(timeInterval);
-        displayMessage();
-    }
-  }, 1000);
-}
-
+// Quiz Questions
 let questions = [
   {
     question : "What does HTML stand for?",
@@ -46,19 +20,19 @@ let questions = [
     correct : "A"
   },
   {
-    question : "What tag is uaded to define a hyperlink, or link to another page",
-    choiceA : "<strong></strong>",
-    choiceB : "<a></a>",
-    choiceC : "<blockquote></blockquote>",
-    choiceD : "<em></em>",
+    question : "What tag is used to define a hyperlink, or link to another page?",
+    choiceA : "strong",
+    choiceB : "a",
+    choiceC : "blockquote",
+    choiceD : "em",
     correct : "B"
   },
   {
     question : "What tag is required in all HTML documents, and is used to define the title?",
-    choiceA : "<br></br>",
-    choiceB : "<body></body>",
-    choiceC : "<head></head>",
-    choiceD : "<title></title>",
+    choiceA : "br",
+    choiceB : "body",
+    choiceC : "head",
+    choiceD : "title ",
     correct : "D"
   },
   {
@@ -119,5 +93,71 @@ let questions = [
   },
 ];
 
-startBtn.onclick = countdown;
+// variables used to de
+let lastQuestionIndex = questions.length - 1;
+let runningQuestionIndex = 0;
+
+
+// Timer that counts down from 75
+let countdown = function() {
+  
+  // setInterval to 1000ms. timer changes color to alert user that time is running out.
+  let timeInterval = setInterval(function() {
+    if( timeLeft > 30){
+        timerEl.innerHTML = "<h4 id='timer'>Time Left: <span style='color: darkgreen;'>" + timeLeft + "</span></h4>";
+        
+        timeLeft--;
+    }else if(timeLeft > 10){
+        timerEl.innerHTML = "<h4 id='timer'>Time Left: <span style='color: orange;'>" + timeLeft + "</span></h4>";
+
+        timeLeft--;
+    }else if(timeLeft >= 1){
+        timerEl.innerHTML = "<h4 id='timer'>Time Left: <span style='color: red;'>" + timeLeft + "</span></h4>";
+
+        timeLeft--;
+    }else{
+        timerEl.textContent = 'Timer: 0';
+        clearInterval(timeInterval);
+        displayMessage();
+    }
+  }, 1000);
+}
+
+
+// Checks to see if user answer is correct
+let checkAnswer = function(answer){
+  if(questions[runningQuestionIndex].correct === answer){
+    alert("Correct!");
+    runningQuestionIndex++
+    renderQuestion();
+  }else{
+    alert("Wrong!");
+    runningQuestionIndex++
+    renderQuestion();
+    timeLeft-=10;
+  }
+}
+
+//renders questions for user
+let renderQuestion = function(){
+  let q = questions[runningQuestionIndex];
+  questionEl.innerHTML = "<h2>" + q.question + "</h2>";
+  choiceA.innerHTML = "<button class='btn'>"+q.choiceA+"</button>";
+  choiceB.innerHTML = "<button class='btn'>"+q.choiceB+"</button>";
+  choiceC.innerHTML = "<button class='btn'>"+q.choiceC+"</button>";
+  choiceD.innerHTML = "<button class='btn'>"+q.choiceD+"</button>";
+
+
+}
+
+let startQuiz = function(){
+  mainEl.style.display = "none";
+  countdown();
+  renderQuestion();
+  quizEl.style.display = "block";
+  
+}
+
+
+startBtn.onclick = startQuiz;
   
